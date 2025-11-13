3c43d73b5ef6fe57f9ea83fe96e5cc204785b877
Marcus Lima <158849178+marcuslsza@users.noreply.github.com>
2025-11-13 16:05:22 -0300

    Update paleta.h

--- proj-itp/src/paleta/paleta.h
+++ proj-itp/src/paleta/paleta.h
@@ -36,27 +36,3 @@ class Paleta {
         return cores[indice];
     }
 };
-#include "sequencia.h"
-
-class Cor{
-    int r, g, b;
-};
-
-class Paleta{
-    Sequencia<Cor> cores;
-    int tamanho;
-
-
-    public:
-
-    Paleta(){
-        tamanho = 0;
-    };
-
-
-    int obterTamanho(void){
-        return tamanho;
-    }
-
-
-};
