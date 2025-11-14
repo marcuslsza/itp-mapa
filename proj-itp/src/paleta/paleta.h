b43d57ad5462b3bbb7b3f6662a30973dc0bbe0d3
Gabriel-Soares-Gomes <gabrielsoaresgomes14@gmail.com>
2025-11-13 23:00:40 -0300

    paleta finalizada

--- proj-itp/src/paleta/paleta.h
+++ proj-itp/src/paleta/paleta.h
@@ -10,33 +10,32 @@ struct Cor{
 
 class Paleta {
     Sequencia<Cor> cores;
-    int tamanho;
+    int tamanho = 0;
     
-
-
     Cor stringParaCor(std::string hexa){
         Cor resultado;
         int posicao_inicial = 1;
+        int valor_int;
 
-        for(int i = 1; i < 4; i++){
+        for(int i = 0; i < 3; i++){
             int soma = 0;
-            int valor_int;
+            int expoente = 1;
             for(int j = posicao_inicial; j < (posicao_inicial + 2) ; j++){
-                int expoente = 1;
-                if((hexa[j] >= 48) || (hexa[j] <= 57)){
+                if((hexa[j] >= 48) && (hexa[j] <= 57)){
                     valor_int = (hexa[j] - 48) * pow(16, expoente);
                 }
-                if((hexa[j] >= 97) || (hexa[j] <= 122)){
+                if((hexa[j] >= 97) && (hexa[j] <= 122)){
                     valor_int = (hexa[j] - 87) * pow(16, expoente);
                 }
                 expoente--;
                 soma += valor_int;
             }
+            
             posicao_inicial += 2;
-            if(i == 1){
+            if(i == 0){
                 resultado.r = soma;
             }
-            else if (i == 2)
+            else if (i == 1)
             {
                 resultado.g = soma;
             }
@@ -48,10 +47,10 @@ class Paleta {
     }
 
     public:
-    Paleta() {
-        tamanho = 0;
-    }
 
+    Paleta() = default;
+
+    
     Paleta(std::string arquivo){
         std::ifstream nomeArquivo(arquivo);
         std::string temp = "";
