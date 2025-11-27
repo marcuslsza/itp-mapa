7eca5dce1bc2b0365e871d7d812f219ab7021b11
Marcus <marcuslimalsza@gmail.com>
2025-11-27 13:55:09 -0300

    iniciando terreno

--- proj-itp/src/terreno/terreno.h
+++ proj-itp/src/terreno/terreno.h
@@ -1,18 +1,29 @@
 #pragma once
 #include <cstdlib>
 #include <cmath>
+#include <ctime>
 #include "../imagem/imagem.h"
 #include "../../../doctest.h"
 
 class Terreno {
     Matriz<int> *terreno;
-
+    int medida;
     public:
 
     Terreno(int n)
     {
-        int medida = pow(2, n) + 1;
+        medida = pow(2, n) + 1;
+        //Cria novo terreno quadrado de tamanho 2^n + 1
         terreno = new Matriz<int>(medida,medida);
+        
+        //Define os valores aleatórios dos 4 cantos do terreno;
+        srand(time(0));
+        
+        //Intervalo de alturas: [1, 10]
+        terreno[0] = rand()%10+1;
+        terreno[medida] = rand()%10+1;
+        terreno[medida*(medida-1)] = rand()%10+1;
+        terreno[medida*(medida-1) + (medida-1)] = rand()%10+1;
     }
 
     ~Terreno() 
@@ -29,4 +40,20 @@ class Terreno {
     {
         return terreno->obterAltura();
     }
-};
+
+    Matriz<int>& operator() (int lin, int col)
+    {
+        //Retorna uma célula específica da matriz terreno(l, c)
+        return terreno[(lin*medida)+col];
+    }
+
+    void diamondStep()
+    {
+        
+    }
+
+    void squareStep()
+    {
+
+    }
+};                          
--- proj-itp/src/terreno/terreno_testes.cpp
+++ proj-itp/src/terreno/terreno_testes.cpp
@@ -1,6 +1,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "../../../doctest.h"
 #include "terreno.h"
+#include <iostream>
 
 TEST_CASE("Testa a criação de um terreno de uma única célula") {
   Terreno terreno(0); // 2^0 + 1 = 2
@@ -14,6 +15,9 @@ TEST_CASE("Testa a geração aleatória de altitudes") {
   CHECK(terreno.obterProfundidade() == 5);
 }
 
+TEST_CASE("Testa a etapa Diamond") {
+  Terreno terreno(2);
+}
 
 // Você precisará criar testes adicionais para cobrir os métodos privados da classe.
 // Por exemplo, você pode criar testes para os métodos das etapas Square e Diamond
