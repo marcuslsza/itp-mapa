dc7fa039dba07045351ba99a2714d7cdc22b4df9
Gabriel-Soares-Gomes <gabrielsoaresgomes14@gmail.com>
2025-11-27 18:07:05 -0300

    criando funcao matrizInicial

--- proj-itp/src/terreno/terreno.h
+++ proj-itp/src/terreno/terreno.h
@@ -4,30 +4,37 @@
 #include <cstdlib>
 #include <ctime>
 #include <cmath>
+#include <iostream>
 
 class Terreno{
 
     Matriz<int> *terreno;
-
+    int dimensao;
     public:
 
     Terreno(int n){
-        int dimensao =  pow(2, n) + 1;
+        dimensao =  pow(2, n) + 1;
         terreno = new Matriz<int>(dimensao, dimensao);
+        srand(time(0));
+        matrizInicial();
     }
 
     ~Terreno() {
         delete terreno;
     }
 
-    int& operator()(int l, int c){
-        int indice = (l*(*terreno).colunas) + c;
+    int& operator()(int linha, int coluna){
+        int indice = ((linha*dimensao) + coluna); //1*2 +0
+        std::cout << indice << std::endl;
+        std::cout << terreno->obterElemento(indice) << std::endl;
+        return terreno->obterElemento(indice);
     }
 
     void matrizInicial(){
-        srand(time(0));
-        
-
+        (*this)(0, 0) = 10;//rand() % 10;
+        (*this)(0, dimensao - 1) = 10; //rand() % 10;
+        (*this)(dimensao - 1, 0) = 10;//rand() % 10;
+        (*this)((dimensao - 1), (dimensao - 1)) = 10;//rand() % 10;
     }
 
     int obterLargura(){
--- proj-itp/src/terreno/terreno_testes.cpp
+++ proj-itp/src/terreno/terreno_testes.cpp
@@ -6,12 +6,20 @@ TEST_CASE("Testa a criação de um terreno de uma única célula") {
   Terreno terreno(0); // 2^0 + 1 = 2
   CHECK(terreno.obterLargura() == 2);
   CHECK(terreno.obterProfundidade() == 2);
+  CHECK(terreno(0,0) == 10);
+  CHECK(terreno(0,1) == 10);
+  CHECK(terreno(1,0) == 10);
+  CHECK(terreno(1,1) == 10);
 }
 
 TEST_CASE("Testa a geração aleatória de altitudes") {
   Terreno terreno(2);
   CHECK(terreno.obterLargura() == 5);
   CHECK(terreno.obterProfundidade() == 5);
+  CHECK(terreno(0,0) == 10);
+  CHECK(terreno(0,4) == 10);
+  CHECK(terreno(4,0) == 10);
+  CHECK(terreno(4,4) == 10);
 }
 
 
